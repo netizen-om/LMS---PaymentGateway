14afe7665bf1329e5e0d348e43cf958dcf7047e8
netizen-om <borisagarom@gmail.com>
2025-03-23 00:04:10 +0530

    added helmet and express-mongo-sanitize for security

--- index.js
+++ index.js
@@ -1,11 +1,27 @@
 import dotenv from "dotenv"
 import morgan from "morgan"
+import rateLimit from "express-rate-limit"
+import mongoSanitizer from "express-mongo-sanitize" 
 dotenv.config()
 
 
 import express from "express"
+import helmet from "helmet"
 const app = express()
-const port = process.env.PORT
+const PORT = process.env.PORT
+
+//Global rate limiting
+const limiter = rateLimit({
+    windowMs: 15 * 60 * 1000, // 15 min
+	limit: 100, // Limit each IP to 100 requests per `window` (here, per 15 minutes).
+	message : "Too many request this IP, you may try again later"
+});
+
+//Security Middleware
+app.use('/api', limiter); // only apply rate limit to routes which starts from "/api"
+app.use(helmet());
+app.use(mongoSanitizer());
+
 
 //body Parser middleware
 app.use(express.json( {limit : '10kb'} ))
@@ -39,4 +55,4 @@ app.use((req,res) => {
 app.get('/', (req, res) => {
     res.send('Hello World!')
 })
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${PORT}!`))

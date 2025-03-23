4f1db041c8478d28badc833279da8d86eeabe389
netizen-om <borisagarom@gmail.com>
2025-03-23 10:42:07 +0530

    added CORS and cookieParser in application

--- index.js
+++ index.js
@@ -2,11 +2,13 @@ import dotenv from "dotenv"
 import morgan from "morgan"
 import rateLimit from "express-rate-limit"
 import mongoSanitizer from "express-mongo-sanitize" 
-dotenv.config()
-
-
 import express from "express"
 import helmet from "helmet"
+import hpp from "hpp"
+import cookieParser from "cookie-parser"
+import cors from "cors"
+
+dotenv.config()
 const app = express()
 const PORT = process.env.PORT
 
@@ -20,17 +22,37 @@ const limiter = rateLimit({
 //Security Middleware
 app.use('/api', limiter); // only apply rate limit to routes which starts from "/api"
 app.use(helmet());
+app.use(hpp());
 app.use(mongoSanitizer());
+
+//CORS configuration
+app.use(cors({
+    origin: process.env.CLIENT_URL || "http://localhost:5173",
+    credentials : true,
+    methods : ["GET", "POST", "PUT", "PATCH", "DELETE", "HEAD", "OPTIONS"],
+    allowedHeaders: [
+        "Content-Type",
+        "Authorization",
+        "X-Requested-With",
+        "device-remember-token",
+        "Access-Control-Allow-Origin",
+        "Origin",
+        "Accept",
+    ],
+}))
 
 
 //body Parser middleware
 app.use(express.json( {limit : '10kb'} ))
 app.use(express.urlencoded( {extended : true, limit : '10kb'} ))
+app.use(cookieParser())
 
 //logging middleware
 if(process.env.NODE_ENV = 'development'){
     app.use(morgan('dev'))
 }
+
+
 
 //Global error handler
 app.use((err, req, res, next) => {
@@ -45,14 +67,16 @@ app.use((err, req, res, next) => {
 }) 
 
 
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+})
+
+// Route not found sould be at end of all routes in express application
 app.use((req,res) => {
     res.status(400).json(
         {status : "error",
         message : "Route not found"}
     )
 })
-
-app.get('/', (req, res) => {
-    res.send('Hello World!')
-})
-app.listen(port, () => console.log(`Example app listening on port ${PORT}!`))
+app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`))

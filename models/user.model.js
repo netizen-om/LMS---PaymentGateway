2bd501bdb9a45ab175188fc6726345cb622ac177
netizen-om <borisagarom@gmail.com>
2025-03-25 20:08:17 +0530

    fixes in user model

--- models/user.model.js
+++ models/user.model.js
@@ -66,7 +66,9 @@ const userSchema = new mongoose.Schema({
         default : Date.now
     },
 }, {
-    timestamps : true
+    timestamps : true,
+    toJSON : { virtuals : true },
+    toObject : { virtuals : true }, 
 })
 
 //hashing Password

979fe1f227e7ce71d17a0182748f9b9947ed91b6
netizen-om <borisagarom@gmail.com>
2025-03-25 20:23:18 +0530

    added basic field in courses schema

---
+++ models/courses.model.js
@@ -0,0 +1,66 @@
+import mongoose from "mongoose";
+
+const curseSchema = new mongoose.Schema({
+    title : {
+        type : String,
+        required : [true, "Course title is required"],
+        trim : true,
+        maxLength : [100, "Course title cannot exceed 100 character"],
+    },
+    subtitle : {
+        type : String,
+        trim : true,
+        maxLength : [200, "Course subtitle cannot exceed 200 character"],
+    },
+    description : {
+        type : String,
+        trim : true,
+    },
+    category : {
+        type : String,
+        required : [true, "Course Category is required"],
+        trim : true,
+    },
+    level : {
+        type : String,
+        enum : {
+            values : ["beginner", "intermediate", "advanceed"],
+            message : 'Please select valid course level'
+        },
+        default : "beginner"
+    },
+    price : {
+        type : Number,
+        required : [true, "Course price is required"],
+        mixLength : [0, "Course price must be not-negative number"],
+    },
+    thumbnail : {
+            type : String,
+            required : [true, "Course thumbnail is required"]
+    },
+    enrolledStudent : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "User"
+        }
+    ],
+    lectures : [
+        {
+            type : mongoose.Schema.Types.ObjectId,
+            ref : "Lecture"
+        }
+    ],
+    instructor : {
+        type : mongoose.Schema.Types.ObjectId,
+        ref : "User",
+        required : [true, "Course instructor is required"]
+    },
+    isPublished : {
+        type : Boolean,
+        default : false
+    },
+    totalDuration : {
+        type : Number,
+        default : 0 
+    }
+})

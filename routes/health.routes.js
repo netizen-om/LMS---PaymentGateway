6053069c0d74d77e484d1daa5c15223cebc9ab4a
netizen-om <borisagarom@gmail.com>
2025-03-28 19:57:38 +0530

    added checkHealth router

---
+++ routes/health.routes.js
@@ -0,0 +1,8 @@
+import express from "express"
+import { checkHealth } from "../controller/health.controller"
+
+const router = express.Router()
+
+router.get("/", checkHealth);
+
+export default router;

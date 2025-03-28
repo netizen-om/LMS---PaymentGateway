a59f3c3794f4cb0b934d9b546402d74812560854
netizen-om <borisagarom@gmail.com>
2025-03-28 19:55:39 +0530

    added checkHealth controller

--- controller/health.controller.js
+++ controller/health.controller.js
@@ -2,7 +2,41 @@ import { getDBStatus } from "../database/db"
 
  
  export const checkHealth = async(req, res) => {
-    getDBStatus();
+    try {
+        const dbstatus = getDBStatus();
+    
+        const healthStatus = {
+            status : "OK",
+            timeStamp : new Date().toISOString(),
+            services : {
+                datebase : {
+                    status : dbstatus.isconnected ? "healthy" : "unhealthy",
+                    details : {
+                        ...dbstatus,
+                        readyState : getReadyStateText(dbstatus.readyState)
+                    }
+                },
+                server : {
+                    status : "healthy",
+                    uptime : process.uptime(),
+                    memoryUsage : process.memoryUsage(),
+                }
+    
+            }
+        }
+    
+        const httpStatus = healthStatus.services.datebase.status === "healthy" ? 200 : 503
+    
+        res.status(httpStatus).json(healthStatus)
+    } catch (error) {
+        console.error("Health check failed");
+        res.status(500).json({
+            status : "ERROR",
+            timeStamp : new Date().toISOString(),
+            error : error.message
+        })
+    }
+
  }
 
 

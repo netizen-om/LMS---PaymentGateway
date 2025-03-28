19b0e6d05a0b06f770e6f14c43574f9cede8da44
netizen-om <borisagarom@gmail.com>
2025-03-28 19:44:20 +0530

    added getReadyStateText in health controller

--- controller/health.controller.js
+++ controller/health.controller.js
@@ -1,3 +1,18 @@
+ 
+ 
+ // This is just small utility function That take number insted of string from fronted
  function getReadyStateText(state) {
+    switch (state) {
+        case 0:
+            return "disconnected"
+        case 1:
+            return "connected"
+        case 2:
+            return "connecting"
+        case 3:
+            return "disconnecting"
     
+        default:
+            return "unknown"
+    }
  }

264f53cde3df30fd14295cfaa75e4a4450e93c34
netizen-om <borisagarom@gmail.com>
2025-03-29 13:33:47 +0530

    added user.controller.js

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,5 +1,6 @@
 import { catchAsync } from "../middleware/error.middleware.js";
+import { User } from "../models/user.model.js";
 
 export const createUserAccount = catchAsync(async(req, res) => {
-    
+    const { name, email, password, role="student" } = req.body;
 })

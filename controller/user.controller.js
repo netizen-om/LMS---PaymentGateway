b6da4a7b929385afe06d94b17d861af9aae04af8
netizen-om <borisagarom@gmail.com>
2025-03-29 13:44:01 +0530

    added createUserAccount in user controller

--- controller/user.controller.js
+++ controller/user.controller.js
@@ -1,6 +1,24 @@
-import { catchAsync } from "../middleware/error.middleware.js";
+import { ApiError, catchAsync } from "../middleware/error.middleware.js";
 import { User } from "../models/user.model.js";
+import { generateToken } from "../utils/generateToken.js";
+
 
 export const createUserAccount = catchAsync(async(req, res) => {
     const { name, email, password, role="student" } = req.body;
+
+    const exsistingUser = await User.findOne({ email : email.toLowerCase() })
+    if(exsistingUser){
+        throw new ApiError("User already exists", 400);
+    }
+
+    const user = await User.create({
+        email : email.toLowerCase(),
+        name,
+        password,
+        role
+    })
+
+    await user.updateLastActive()
+    generateToken(res, user, "Account created successfully")
+    
 })

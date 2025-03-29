6cf3c734d84b61f1a5b1abd937e0ae72a9be0470
netizen-om <borisagarom@gmail.com>
2025-03-29 13:29:34 +0530

    added error middleware

--- routes/health.routes.js
+++ routes/health.routes.js
@@ -1,5 +1,5 @@
 import express from "express"
-import { checkHealth } from "../controller/health.controller"
+import { checkHealth } from "../controller/health.controller.js"
 
 const router = express.Router()
 

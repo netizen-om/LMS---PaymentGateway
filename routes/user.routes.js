5b9b3017f0dbf7c124b5520075c34742e6e27574
netizen-om <borisagarom@gmail.com>
2025-04-26 16:12:35 +0530

    added patch req. to upload avatar

--- routes/user.routes.js
+++ routes/user.routes.js
@@ -1,7 +1,7 @@
 import express from "express"
-import { authenticateUser, createUserAccount, getCurrentUserProfile, signOutUser } from "../controller/user.controller.js";
+import { authenticateUser, createUserAccount, getCurrentUserProfile, signOutUser, updateUserProfile } from "../controller/user.controller.js";
 import { isAuthenticated } from "../middleware/auth.middleware.js";
-createUserAccount
+import upload from "../utils/multer.js";
 
 const router = express.Router()
 
@@ -12,6 +12,7 @@ router.get("/signout", signOutUser);
 
 // Profile routes : PROTECTED :
 router.get("/profile", isAuthenticated, getCurrentUserProfile);
+router.patch("/profile", upload.single("avatar") , isAuthenticated, updateUserProfile);
 
 
 export default router;
